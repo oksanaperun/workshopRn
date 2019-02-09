742c51d7537faec77fb273b3e6a18250271b644a
Ivan Iankovskyi <ivaniankovskyi@XPUAKYIW689402.kyiv.epam.com>
2019-02-09 10:24:23 +0200

    add navigation

--- index.js
+++ index.js
@@ -3,8 +3,30 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
+ import React from 'react';
+import { AppRegistry, View, Text } from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// In App.js in a new project
+
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  }
+});
+
+AppRegistry.registerComponent(appName, () => createAppContainer(AppNavigator));
